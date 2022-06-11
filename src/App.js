1285ac478357310d597ab26540230735e760ed32
Betselot Bezaebeh <betselotsahilu@gmail.com>
2022-06-11 14:46:49 -0500

    add semantics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(postion) {
+      setLat(postion.coords.latitude);
+      setLong(postion.coords.longitude);
+    }
+      );
+    console.log("Latitude is:", lat)
+    console.log("Longitude is", long)
+  }, [lat, long]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }

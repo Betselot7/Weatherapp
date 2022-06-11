3b61ebac06687b04ec036797c9de04f34a9b8c4b
Betselot Bezaebeh <betselotsahilu@gmail.com>
2022-06-11 15:09:50 -0500

    add a console log line

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import React, {useEffect, useState} from "react";
     console.log("Latitude is:", lat)
     console.log("Longitude is", long)
     console.log("Longitude is", long)
+    console.log("Longitude is", long)
   }, [lat, long]);
 
 function App() {
